04e25a2f768201109c2447eaabbfb6a5607eb60d
phamvlap <phamvlap302@gmail.com>
2024-10-10 01:21:58 +0700

    feat: add some util functions

--- nmt/utils.py
+++ nmt/utils.py
@@ -1,41 +1,15 @@
-import torch
+import torch.nn as nn
+import torch.optim as optim
 
-from torch import Tensor
 from torch.utils.data import Dataset
+from pathlib import Path
 
 from nltk.translate import bleu_score
 from datasets import load_dataset, Dataset as DatasetModel
 
-
-# Create a tensor with zeros above the diagonal, ones below and on the diagonal
-def causal_mask(size: Tensor) -> Tensor:
-    mask = torch.triu(torch.ones(1, size, size), diagonal=1).to(dtype=torch.int64)
-    return (mask == 0).int()
-
-
-def create_encoder_mask(
-    encoder_input: Tensor,
-    pad_token_id: Tensor,
-    device: str = "cpu",
-) -> Tensor:
-    return (
-        (encoder_input != pad_token_id)
-        .unsqueeze(dim=0)
-        .unsqueeze(dim=0)
-        .int()
-        .to(device=device)
-    )
-
-
-def create_decoder_mask(
-    decoder_input: Tensor,
-    pad_token_id: Tensor,
-    device: str = "cpu",
-) -> Tensor:
-    return (
-        (decoder_input != pad_token_id).unsqueeze(dim=0).unsqueeze(dim=0).int()
-        & causal_mask(size=decoder_input.size(0))
-    ).to(device=device)
+ADAM = "adam"
+ADAMW = "adamw"
+NOAM_DECAY = "noam"
 
 
 def load_data(config: dict) -> Dataset:
@@ -59,15 +33,87 @@ def load_data(config: dict) -> Dataset:
     return out
 
 
-def calc_bleu_score(cans: list[str], refs: list[list[str]]) -> list[float]:
+def calc_bleu_score(cands: list[str], refs: list[list[str]]) -> list[float]:
     scores = []
     for i in range(1, 5):
         weights = tuple([1 / i for _ in range(i)])
         scores.append(
             bleu_score.corpus_bleu(
                 list_of_references=refs,
-                hypotheses=cans,
+                hypotheses=cands,
                 weights=weights,
             )
         )
     return scores
+
+
+def make_optimizer(model: nn.Module, config: dict) -> optim.Optimizer:
+    optimizer_type = config["optimizer"].strip().lower()
+
+    if optimizer_type == ADAM:
+        optimizer = optim.Adam(
+            model.parameters(),
+            lr=config["lr"],
+            betas=config["betas"],
+            eps=config["eps"],
+            weight_decay=config["weight_decay"],
+        )
+    elif optimizer_type == ADAMW:
+        optimizer = optim.AdamW(
+            model.parameters(),
+            lr=config["lr"],
+            betas=config["betas"],
+            eps=config["eps"],
+            weight_decay=config["weight_decay"],
+        )
+    else:
+        raise ValueError(
+            f"Unknown optimizer type {optimizer_type}. Supported types are: {ADAM}, {ADAMW}"
+        )
+    return optimizer
+
+
+def noam_decay(
+    model_size: int,
+    step: int,
+    warmup_steps: int,
+    factor: float = 1.0,
+) -> float:
+    step = max(1, step)
+    return factor * (
+        model_size ** (-0.5) * min(step ** (-0.5), step * warmup_steps ** (-1.5))
+    )
+
+
+def get_lr_scheduler(
+    optimizer: optim.Optimizer,
+    config: dict,
+) -> optim.lr_scheduler.LRScheduler:
+    lr_scheduler_type = config["lr_scheduler"].strip().lower()
+    lr_scheduler = None
+
+    if lr_scheduler_type == NOAM_DECAY:
+        lr_scheduler = optim.lr_scheduler.LambdaLR(
+            optimizer=optimizer,
+            lr_lambda=lambda step: noam_decay(
+                model_size=config["d_model"],
+                step=step,
+                warmup_steps=config["warmup_steps"],
+            ),
+        )
+
+    return lr_scheduler
+
+
+def get_weights_file_path(model_folder: str, model_basename: str, epoch: str) -> str:
+    return f"{model_folder}/{model_basename}{epoch}.pt"
+
+
+def get_list_weights_file_paths(config: dict) -> list[str] | None:
+    model_folder = config["model_folder"]
+    model_basename = config["model_basename"]
+    weight_filepaths = list(Path(model_folder).glob(f"{model_basename}*.pt"))
+    if len(weight_filepaths) == 0:
+        return None
+    weight_filepaths = [str(file) for file in sorted(weight_filepaths)]
+    return weight_filepaths
